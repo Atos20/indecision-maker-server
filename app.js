7741c22719c9601744aa2dca3806b77276db1d2d
Will Mitchell <wvmitchell@gmail.com>
2019-03-14 15:18:48 -0600

    add bodyParser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.locals.title = 'Test Express';
+
+app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
   response.send('We\'re going to test all the routes!');
