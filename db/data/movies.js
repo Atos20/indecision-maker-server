ea6cebd44eb6de599d250bd165c46d8966edec13
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 18:35:46 -0400

    Adjust reduce to be async

--- db/data/movies.js
+++ db/data/movies.js
@@ -1,8 +1,14 @@
-const movieFetch = require('../../apiCalls.js')
+const apiCalls = require("../../apiCalls.js")
 
-const movieData = async (table) => {
-  const dataToPopulate = await movieFetch(table)
-  return dataToPopulate
+const movieData = async (genre) => {
+  const moviesByGenre = await apiCalls.movieFetch(genre)
+  const scaledDownMovies = moviesByGenre.slice(0, 25)
+  return dataToPopulate.reduce(async (acc, cur) => {
+    let randomMovieTitle = cur.title.split('/')[2]
+    let randomMovieDetails = await apiCalls.singleMovie(randomMovieTitle)
+    acc.push(randomMovieDetails)
+    return acc
+  })
 }
 
 module.exports = movieData;
