2a2db0f101f518d29dcff6af157512cc45dd3964
Orlando Murcio <ato.murcio@gmail.com>
2020-10-28 16:36:09 -0600

    Fix typo and delete students table'

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/indicision',
+    connection: 'postgres://localhost/indecision',
     migrations: {
       directory: './db/migrations'
     },
