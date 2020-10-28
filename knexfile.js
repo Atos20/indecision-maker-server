09622d20b84259c297f70efb7584dd536cd36bce
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-28 15:32:24 -0600

    Change database from school to activities in development.connection

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/school',
+    connection: 'postgres://localhost/activities',
     migrations: {
       directory: './db/migrations'
     },
