74de0ec8f3c963c2f53c2ce87150fbe6758cc93c
Orlando Murcio <ato.murcio@gmail.com>
2020-10-28 15:32:39 -0600

    Define initial podcast schema

--- db/migrations/20201028150700_podcasts.js
+++ db/migrations/20201028150700_podcasts.js
@@ -2,7 +2,17 @@
 exports.up = function (knex) {
     return knex.schema
       .createTable('podcasts', function (table) {
-   
+        table.increment('id');
+        table.string('title').notNullable();
+        table.integer('author').notNullable();
+        table.integer('audience').notNullable();
+        table.integer('genre').notNullable();
+        table.integer('episode_count').notNullable();
+        table.string('collectionViewUrl').notNullable();
+        table.integer('episode_count').notNullable();
+        table.string('release_date').notNullable();
+        table.string('image_100').notNullable();
+        table.string('image_600').notNullable();
       })
   };
   
