e23db784f1b34a48ec88c99cbd26adca7c1604e8
Orlando Murcio <ato.murcio@gmail.com>
2020-10-28 16:35:26 -0600

    Remove extra line of code

--- db/migrations/20201028150700_podcasts.js
+++ db/migrations/20201028150700_podcasts.js
@@ -2,17 +2,16 @@
 exports.up = function (knex) {
     return knex.schema
       .createTable('podcasts', function (table) {
-        table.increment('id');
-        table.string('title').notNullable();
-        table.integer('author').notNullable();
-        table.integer('audience').notNullable();
-        table.integer('genre').notNullable();
+        table.increments('id').primary();
+        table.string('title', 50).notNullable();
+        table.string('author', 50).notNullable();
+        table.string('audience', 50).notNullable();
+        table.string('genre', 50).notNullable();
+        table.string('collectionViewUrl');
         table.integer('episode_count').notNullable();
-        table.string('collectionViewUrl').notNullable();
-        table.integer('episode_count').notNullable();
-        table.string('release_date').notNullable();
-        table.string('image_60').notNullable();
-        table.string('image_100').notNullable();
+        table.string('release_date', 50).notNullable();
+        table.string('image_60');
+        table.string('image_100');
       })
   };
   
