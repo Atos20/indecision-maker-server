06a08eae5573dce5b4b0624fa71775e61b2914de
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 19:26:40 -0400

    Add new properties to seed for movie

--- db/seeds/dev/moviesSeed.js
+++ db/seeds/dev/moviesSeed.js
@@ -5,7 +5,13 @@ const category = 'family'
 const createMovie = async (knex, movie) => {
   const movieId = await knex('movies').insert({
     title: movie.title.title,
-
+    brief_description: movie.plotSummary.text,
+    content_rating: movie.certificates.US.certificate,
+    image_poster: movie.title.image.url,
+    imdb_rating: movie.rating.rating,
+    release_date: movie.releaseDate,
+    runtime: movie.title.runningTimeInMinutes,
+    where_to_watch: "not sure",
   }, 'id');
 }
 exports.seed = async (knex) => {
