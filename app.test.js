46b8063d991e1cb55659fcb1aab9f1aca3ead301
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-28 11:43:09 -0600

    Create unit test for successful fetch of all movies

--- app.test.js
+++ app.test.js
@@ -2,6 +2,10 @@ import "@babel/polyfill";
 import request from 'supertest'
 import app from './app'
 
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
 describe('Server', () => {
   describe('init', () => {
     it('should return a 200 status', async () => {
@@ -10,3 +14,16 @@ describe('Server', () => {
     });
   });
 });
+
+describe('GET /api/v1/movies', () => {
+  it('should return a 200 and all of the movies', async () => {
+    const expectedMovies = await database('movies').select();
+    const response = await request(app).get('/api/v1/movies');
+    const movies = response.body;
+
+    expect(response.status).toBe(200);
+    expect(movies).toEqual(expectedMovies);
+  })
+
+  //create sad path test
+})
