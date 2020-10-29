c890b75ce8a196e3df7a4b9407c1bee257c6c8e7
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 22:52:17 -0400

    Refactor data to more accurately populate data

--- db/data/board_games.js
+++ db/data/board_games.js
@@ -1,20 +1,18 @@
 const apiCalls = require("../../apiCalls.js")
-const games = ['chess', 'jenga', 'clue', 'scrabble', 'connect four', 'candy Land', 'exploding Kittens', 'scrabble', 'life', 'guess Who?', 'operation', 'scategories', 'apples to Apples', 'monopoly', 'battleship', 'mouse trap', 'trivial Pursuit', 'charades', 'pictionary']
+const games = ['chess', 'checkers', 'jenga', 'clue', 'scrabble', 'connect four', 'candy Land', 'exploding Kittens', 'scrabble', 'life', 'guess Who?', 'operation', 'scategories', 'apples to Apples', 'monopoly', 'battleship', 'mouse trap', 'trivial Pursuit', 'charades', 'pictionary']
 
 const board_gameData = async () => {
-  const moviesByGenre = await apiCalls.movieFetch(genre)
-  let moviesToPost = []
+  let gamesToPost = []
   let counter = 0;
-  while (moviesToPost.length < 25) {
-    let randomMovieTitle = moviesByGenre[counter].split('/')[2]
-    let randomMovieDetails = await apiCalls.singleMovie(randomMovieTitle)
-    if (randomMovieDetails) {
+  while (gamesToPost.length < 20) {
+    const gamesInfo = await apiCalls.boardGame(games[counter])
+    if (gamesInfo) {
       moviesToPost.push(randomMovieDetails)
     }
     counter++
-    setTimeout(() => {console.log(`${counter} / 25`)}, 250)
+    setTimeout(() => {console.log(`${counter} / 20`)}, 250)
   }
-  return moviesToPost
+  return gamesToPost
 }
 
 module.exports = movieData;
