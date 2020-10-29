0c14330c39a3a9bfacf1bab5f6b137c44a72c887
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-28 19:52:23 -0600

    Remove single_player from new card object

--- db/seeds/dev/cardgames.js
+++ db/seeds/dev/cardgames.js
@@ -1,11 +1,10 @@
-import cardGamesData from '../../data/cardGamesData.js';
+const cardGamesData = require('../../data/cardGamesData.js');
 
 const createCardGame = async (knex, game) => {
   const newPaper = await knex('card_games').insert({
     name: game.name,
     instructions: game.instructions,
     description: game.description,
-    single_player: game.single_player,
     materials: game.materials,
     number_of_players: game.number_of_players,
     video: game.video,
