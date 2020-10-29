cfabf41faa78d7e3e9aebdc0c8d33790e75febbf
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 20:48:03 -0400

    Remove reduce in favor of while loop

--- db/data/movies.js
+++ db/data/movies.js
@@ -2,13 +2,18 @@ const apiCalls = require("../../apiCalls.js")
 
 const movieData = async (genre) => {
   const moviesByGenre = await apiCalls.movieFetch(genre)
-  const scaledDownMovies = moviesByGenre.slice(0, 25)
-  return dataToPopulate.reduce(async (acc, cur) => {
-    let randomMovieTitle = cur.title.split('/')[2]
+  let moviesToPost = []
+  let counter = 0;
+  while (moviesToPost.length < 25) {
+    let randomMovieTitle = moviesByGenre[counter].split('/')[2]
     let randomMovieDetails = await apiCalls.singleMovie(randomMovieTitle)
-    acc.push(randomMovieDetails)
-    return acc
-  })
+    if (randomMovieDetails) {
+      moviesToPost.push(randomMovieDetails)
+    }
+    counter++
+    setTimeout(() => {console.log('hi')}, 500)
+  }
+  return moviesToPost
 }
 
 module.exports = movieData;
