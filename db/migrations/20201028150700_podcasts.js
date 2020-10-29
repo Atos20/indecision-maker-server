192296226a4f7dc919de14432ead5dbae065c690
Orlando Murcio <ato.murcio@gmail.com>
2020-10-28 20:28:06 -0600

    Update migration file

--- db/migrations/20201028150700_podcasts.js
+++ db/migrations/20201028150700_podcasts.js
@@ -3,17 +3,17 @@ exports.up = function (knex) {
     return knex.schema
       .createTable('podcasts', function (table) {
         table.increments('id').primary();
-        table.string('podcast_id');
         table.string('podcast_name');
         table.string('podcast_title');
         table.string('author');
-        table.string('audience');
+        table.string('familiy_friendly').defaultTo(1);
         table.string('genre');
         table.string('collectionViewUrl');
         table.integer('episode_count');
         table.string('release_date');
         table.string('image_60');
         table.string('image_100');
+        table.timestamps(true, true)
       })
   };
   
