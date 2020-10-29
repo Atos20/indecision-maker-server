a0ec03b296ae24f87b71182e58f4f97806f39b30
Orlando Murcio <ato.murcio@gmail.com>
2020-10-29 10:55:02 -0600

    Move getAllData function to apiCalls file

--- apiCalls.js
+++ apiCalls.js
@@ -44,6 +44,20 @@ const boardGame = async (name) => {
     return e
   }
 }
+
+const getAllData = async (activity) => {
+  try {
+  const response = await fetch(`https://itunes.apple.com/search?term=${activity}`, {
+    "method": "GET",
+  })
+  const resolved = await response.json()
+  return resolved
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
 module.exports = {
-  movieFetch, singleMovie, boardGame
+  movieFetch, singleMovie, boardGame, getAllData
 }
