efeec53c2847a74ee0badc9a54735e7f6c0fbe41
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-29 01:02:31 -0400

    Standardize naming convention

--- db/data/board_games.js
+++ db/data/board_games.js
@@ -1,7 +1,7 @@
 const apiCalls = require("../../apiCalls.js")
 
 const board_gameData = async () => {
-  const games = ['chess', 'checkers', 'jenga', 'clue', 'scrabble', 'connect four', 'candy Land', 'exploding Kittens', 'scrabble', 'life', 'guess who', 'operation', 'Scattergories', 'apples to Apples', 'monopoly', 'battleship', 'mouse trap', 'trivial Pursuit', 'charades', 'pictionary']
+  const games = ['chess', 'checkers', 'jenga', 'clue', 'scrabble', 'connect four', 'candy land', 'exploding kittens', 'scrabble', 'life', 'guess who', 'operation', 'scattergories', 'apples to apples', 'monopoly', 'battleship', 'mouse trap', 'trivial pursuit', 'charades', 'pictionary']
   let gamesToPost = []
   let counter = 0;
   while (gamesToPost.length < 20) {
