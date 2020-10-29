534f2097448e192611a570d4463594dddc3269ab
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 22:42:45 -0400

    Reformat to match card_game schema

--- db/migrations/20201028220558_board_game.js
+++ db/migrations/20201028220558_board_game.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+exports.up = knex => {
+  return knex.schema.createTable('board_games', function (table) {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('instructions', 10000)
+    table.string('description', 1000)
+    table.string('materials')
+    table.string('number_of_players')
+    table.string('image')
+    table.string('where_to_watch').defaultTo(1)
+    table.timestamps(true, true)
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = knex => {
+  return knex.schema.dropTable('board_games')
 };
