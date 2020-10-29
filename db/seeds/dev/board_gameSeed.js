a60ae5153287bbf4d95ac92bf42d6736b869097b
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 23:11:54 -0400

    Adjust insert syntax

--- db/seeds/dev/board_gameSeed.js
+++ db/seeds/dev/board_gameSeed.js
@@ -1,13 +1,26 @@
+const movieData = require('../../data/movies');
+const category = 'family'
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+const createBoardgame = async (knex, movie) => {
+  const boardId = await knex('movies').insert({
+    name: board.name,
+    description: board.description || board.description_preview,
+    min_players: board.min_players,
+    max_players: board.max_players,
+    image: board.image_url,
+    average_time: ((+board.min_playtime + +board.max_playtime) / 2),
+  }, 'id');
+}
+exports.seed = async (knex) => {
+  try {
+    await knex('movies').del()
+    let allMovieData = await movieData(category)
+    let moviePromises = allMovieData.map(movie => {
+      return createMovie(knex, movie);
     });
+    return Promise.all(moviePromises);
+  } catch (error) {
+    console.log(`Error seeding data: ${error}`)
+  }
+};
 };
