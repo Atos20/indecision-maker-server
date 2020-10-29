9e6899d0765e8a865af9249b4b9e8b6aace28e25
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 22:47:02 -0400

    Refactor return on moviecalls, add boardGame fetch

--- apiCalls.js
+++ apiCalls.js
@@ -9,8 +9,8 @@ const movieFetch = async (table) => {
         "x-rapidapi-key": "580db340e1mshd3d03efa046e426p182a53jsnf5a8138fcc1a"
       }
     })
-    const resolved = await response.json()
-    return resolved
+    return await response.json()
+
   }
   catch (e) {
     return e
@@ -25,14 +25,24 @@ const singleMovie = async (title) => {
         "x-rapidapi-key": "580db340e1mshd3d03efa046e426p182a53jsnf5a8138fcc1a"
       }
     })
-    const resolved = await response.json()
-    return resolved
+    return await response.json()
+
+  }
+  catch (e) {
+    return e
+  }
+}
+const boardGame = async (name) => {
+  try {
+    const response = await fetch(`https://www.boardgameatlas.com/api/search?name=${name}&pretty=true&client_id=JLBr5npPhV`, {
+      "method": "GET",
+    })
+    return await response.json()
   }
   catch (e) {
     return e
   }
 }
 module.exports = {
-  movieFetch, singleMovie
-
+  movieFetch, singleMovie, boardGame
 }
