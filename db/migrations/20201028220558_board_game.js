db8904c0df03c42690e2c4e6ecf7a657a597efdf
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-28 23:11:03 -0400

    Refine schema to reflect the data coming in

--- db/migrations/20201028220558_board_game.js
+++ db/migrations/20201028220558_board_game.js
@@ -2,12 +2,11 @@ exports.up = knex => {
   return knex.schema.createTable('board_games', function (table) {
     table.increments('id').primary()
     table.string('name')
-    table.string('instructions', 10000)
-    table.string('description', 1000)
-    table.string('materials')
-    table.string('number_of_players')
+    table.string('description', 10000)
+    table.string('min_players')
+    table.string('max_players')
     table.string('image')
-    table.string('where_to_watch').defaultTo(1)
+    table.string('averate_time').defaultTo(1)
     table.timestamps(true, true)
   })
 };
