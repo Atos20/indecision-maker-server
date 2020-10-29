da4a0d5d4e4553859271ab9c2f5fab5b2b079c2e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-28 19:55:08 -0600

    Change name of connection path to match database reference from main

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/activities',
+    connection: 'postgres://localhost/indecision',
     migrations: {
       directory: './db/migrations'
     },
