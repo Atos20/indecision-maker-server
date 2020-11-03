723acce377932f90875090df343979f37c240ce7
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-02 22:14:36 -0500

    Remove whitespace

--- db/migrations/20201028195920_music.js
+++ db/migrations/20201028195920_music.js
@@ -1,20 +1,18 @@
 exports.up = function (knex) {
-    return knex.schema
-      .createTable('music', function (table) {
-        table.increments('id').primary();
-        table.string('artist');
-        table.string('genre');
-        table.string('song_title');
-        table.string('album_title');
-        table.string('album_url');
-        table.string('type');
-        table.string('release_date');
-        table.string('image_60');
-        table.string('image_100');
-      })
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema
-      .dropTable('music')
-  };
+  return knex.schema.createTable("music", function (table) {
+    table.increments("id").primary();
+    table.string("artist");
+    table.string("genre");
+    table.string("song_title");
+    table.string("album_title");
+    table.string("album_url");
+    table.string("type");
+    table.string("release_date");
+    table.string("image_60");
+    table.string("image_100");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("music");
+};
