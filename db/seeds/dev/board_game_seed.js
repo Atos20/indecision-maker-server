04148b2f5f8ed8d1703f18132886f4e8933debea
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-02 22:34:02 -0500

    Adjust paths and remove boardId, value was never used

---
+++ db/seeds/dev/board_game_seed.js
@@ -0,0 +1,24 @@
+const board_game_data = require('../../data/board_games_data');
+
+const createBoardgame = async (knex, board) => {
+  await knex('board_games').insert({
+    name: board.name,
+    description: board.description || board.description_preview,
+    min_players: board.min_players,
+    max_players: board.max_players,
+    image: board.image_url,
+    average_time: ((+board.min_playtime + +board.max_playtime) / 2),
+  });
+}
+exports.seed = async (knex) => {
+  try {
+    await knex('board_games').del()
+    let boardData = await board_game_data()
+    let boardGamePromises = boardData.map(board => {
+      return createBoardgame(knex, board);
+    });
+    return Promise.all(boardGamePromises);
+  } catch (error) {
+    console.log(`Error seeding data: ${error}`)
+  }
+};
