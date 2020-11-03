fc636e1cfe8a035df633d4a9d06015c061764ce4
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-02 22:59:18 -0500

    Lint

--- app.js
+++ app.js
@@ -1,112 +1,118 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
 
-app.locals.title = 'Test Express';
+app.locals.title = "Test Express";
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/v1/movies', async (req, res) => {
+app.get("/api/v1/movies", async (req, res) => {
   try {
-    const movies = await database('movies').select();
+    const movies = await database("movies").select();
     res.status(200).json(movies);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/movies/:genre', async (req, res) => {
+app.get("/api/v1/movies/:genre", async (req, res) => {
   try {
-    const movies = await database('movies').where('genre', 'ilike', `%${req.params.genre}%`).select();
+    const movies = await database("movies")
+      .where("genre", "ilike", `%${req.params.genre}%`)
+      .select();
     if (movies.length) {
       res.status(200).json(movies);
     } else {
       res.status(404).json({
-        error: `No movies found with a genre of ${req.params.genre}`
-      })
+        error: `No movies found with a genre of ${req.params.genre}`,
+      });
     }
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/boardgames', async (req, res) => {
+app.get("/api/v1/boardgames", async (req, res) => {
   try {
-    const boardGames = await database('board_games').select();
+    const boardGames = await database("board_games").select();
     res.status(200).json(boardGames);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/cardgames', async (req, res) => {
+app.get("/api/v1/cardgames", async (req, res) => {
   try {
-    const cardGames = await database('card_games').select();
+    const cardGames = await database("card_games").select();
     res.status(200).json(cardGames);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/music', async (req, res) => {
+app.get("/api/v1/music", async (req, res) => {
   try {
-    const music = await database('music').select();
+    const music = await database("music").select();
     res.status(200).json(music);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/music/:genre', async (req, res) => {
+app.get("/api/v1/music/:genre", async (req, res) => {
   try {
-    const music = await database('music').where('genre', `${req.params.genre}`).select();
+    const music = await database("music")
+      .where("genre", `${req.params.genre}`)
+      .select();
     if (music.length) {
       res.status(200).json(music);
     } else {
       res.status(404).json({
-        error: `No music found with a genre of ${req.params.genre}`
-      })
+        error: `No music found with a genre of ${req.params.genre}`,
+      });
     }
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/podcasts', async (req, res) => {
+app.get("/api/v1/podcasts", async (req, res) => {
   try {
-    const podcasts = await database('podcasts').select();
+    const podcasts = await database("podcasts").select();
     res.status(200).json(podcasts);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/podcasts/:genre', async (req, res) => {
+app.get("/api/v1/podcasts/:genre", async (req, res) => {
   try {
-    const podcasts = await database('podcasts').where('genre', 'ilike', `%${req.params.genre}%`).select();
+    const podcasts = await database("podcasts")
+      .where("genre", "ilike", `%${req.params.genre}%`)
+      .select();
     if (podcasts.length) {
       res.status(200).json(podcasts);
     } else {
       res.status(404).json({
-        error: `No podcasts found with a genre of ${req.params.genre}`
-      })
+        error: `No podcasts found with a genre of ${req.params.genre}`,
+      });
     }
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
-app.get('/api/v1/questions', async (req, res) => {
+app.get("/api/v1/questions", async (req, res) => {
   try {
-    const questions = await database('questions').select();
+    const questions = await database("questions").select();
     res.status(200).json(questions);
   } catch (e) {
-    res.status(500).json({e})
+    res.status(500).json({e});
   }
-})
+});
 
 module.exports = app;
