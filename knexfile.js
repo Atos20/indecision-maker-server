ce3d94a68a0c1fc92a6639c26fce1e6efd3ed6aa
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-04 14:13:10 -0500

    Add ssl unauthorized rejection to false

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,8 @@ module.exports = {
 
   production: {
   client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
+  connection: process.env.DATABASE_URL `?ssl=true`,
+  ssl: { rejectUnauthorized: false },
   migrations: {
     directory: './db/migrations'
   },
