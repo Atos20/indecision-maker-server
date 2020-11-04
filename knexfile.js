bbbc9caecd27f4556aef3846e15128fe6810cbc9
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-04 13:04:13 -0500

    Adjust knexfile.js for production

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,15 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+},
+
   test: {
     client: 'pg',
     connection: 'postgres://localhost/indecision',
